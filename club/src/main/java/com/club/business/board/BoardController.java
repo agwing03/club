3599ddf4611a7cf57ef661fc09fa0aad1dbd2adf
agwing03 <agwing03@naver.com>
2022-12-02 13:08:01 +0900

    test

--- club/src/main/java/com/club/business/board/BoardController.java
+++ club/src/main/java/com/club/business/board/BoardController.java
@@ -57,8 +57,7 @@ public class BoardController{
 		model.addAttribute("response", boardService.selectBoardInfo(params));
 		model.addAttribute("searchVO", params);
 		
-		return "board/boardView";
-		
+		return "board/boardView";		
 	}
 	
 	/**
